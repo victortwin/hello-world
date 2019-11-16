7532296005c59ac75ce240f6b2b4adbf0b89cd95
Victor Tormyshev <lifterr@mail.ru>
2019-11-16 22:57:01 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String hello = "Hello";
+        String world = "world";
+        System.out.println(hello + " " + world + "!");
+    }
+}
